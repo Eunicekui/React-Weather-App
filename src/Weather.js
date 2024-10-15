1da3339cfac844d9a4b11b9370242052de7fcc76
Eunicekui <kamaueunice384@gmail.com>
2024-10-15 15:37:18 +0300

    troubleshooted

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import WeatherInfo from "./WeatherInfo";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from "axios";
 import "./Weather.css";
 
