4f724f32ed3b2ecfbc0aa43a3c23ab90b203094d
Eunicekui <kamaueunice384@gmail.com>
2024-10-15 17:31:23 +0300

    integrated api

--- src/Weather.js
+++ src/Weather.js
@@ -3,6 +3,7 @@ import WeatherInfo from "./WeatherInfo";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from "axios";
 import "./Weather.css";
+import WeatherForecast from "./WeatherForecast";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
@@ -61,6 +62,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
+        <WeatherForecast cordinates={weatherData.coordinates} />
       </div>
     );
   } else {
